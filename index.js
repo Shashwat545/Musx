aec93b6c779121031cf846cbf21f9054c1696508
Shashwat Singh <shashwatsingh545@gmail.com>
2022-03-23 15:11:18 +0530

    Revert "Setup initial website files"
    This reverts commit fabf239a5f8bd7ebe7c4487fd77561d2a5488ece.

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 const {Client, Intents}=require('discord.js');
 const {Player}=require('discord-player');
-const express=require('express');
-const path=require('path');
-const app=express();
-const port=process.env.PORT||3000;
-app.use(express.json());
-app.use(express.static("website"));
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/website/index.html'));
-});
-app.listen(port,()=>{
-    console.log("Website is running on "+port)
-});
 require('dotenv').config();
 global.client=new Client({
     intents: [
